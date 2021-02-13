7800d04832965b097c880bc5f8f51fd35832b5b9
Eliabe klippel de souza <eliabeeks@gmail.com>
2021-02-12 22:23:09 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function calcular() {
+    var number1 = document.getElementById("number1").value
+    var number2 = document.getElementById("number2").value
+    alert(Number(number1) + Number(number2))
+}
