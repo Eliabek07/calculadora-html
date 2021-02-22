25138b12da717eddee8bc103d558ad9e051baeea
eliabe eks <eliabeeks@gmail.com>
2021-02-22 20:50:19 -0300

    criando novas funcoes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 function calcular() {
     var number1 = document.getElementById("number1").value
     var number2 = document.getElementById("number2").value
+
+
     alert(Number(number1) + Number(number2))
+
+}
+
+
+function Subtrair() {
+    var number1 = document.getElementById("number1").value
+    var number2 = document.getElementById("number2").value
+
+
+    alert(Number(number1) - Number(number2))
+
+}
+
+function Multiplicar() {
+    var number1 = document.getElementById("number1").value
+    var number2 = document.getElementById("number2").value
+
+
+    alert(Number(number1) * Number(number2))
+
+}
+
+function Dividir() {
+    var number1 = document.getElementById("number1").value
+    var number2 = document.getElementById("number2").value
+    if (number1 == 0 || number2 == 0) {
+        alert("Não pode ser zero")
+    }
+    else { alert(Number(number1) / Number(number2)) }
+
+
+
 }
